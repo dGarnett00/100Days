960fa2908bcf1e87f1aac9b21189629053adb15b
dGarnett00 <davonta2222@gmail.com>
2024-10-10 18:31:16 -0400

    added comments to ifstatements.py

--- ifStatements.py
+++ ifStatements.py
@@ -3,4 +3,5 @@ if myName == "David":
     print("Welcome DUde!")
     print("You are welcome!")
 else:
-    print("Sorry, but you are not David.")
+    print("Sorry, but you are not David.")        #Single "=" changes something #single "=" 
+                                                # Double "=" assigns a value to a variable
