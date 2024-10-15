d30f819ba5195ce5a6be8bb3a79d5576c565d3a6
dGarnett00 <davonta2222@gmail.com>
2024-10-15 11:16:39 -0400

    Nesting with pyhton

---
+++ nesting.py
@@ -0,0 +1,21 @@
+tvShow = input("What is your favorite tv show? ")
+
+
+if tvShow == "hockey" :
+    print("hockey is the best!")
+    favCharacter = input("Who is your favorite hockey player? ")
+    if favCharacter == "me" :
+        print("bro, you wack!")
+    else:
+        print("best answer")
+  
+  
+  
+elif tvShow == "football":
+    print("hmmmm, Why is that?")
+ 
+ 
+ 
+ 
+else:
+    print("Shoud have said hockey or football.")
