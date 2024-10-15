adcbd4c7c52569f1e0083ee9a4e7dfb7ca40e807
dGarnett00 <davonta2222@gmail.com>
2024-10-15 10:23:14 -0400

    Else if Statments

---
+++ ELif.py
@@ -0,0 +1,9 @@
+print("Secure login")
+username = input("Username > ")
+if username == "mark":
+    print("Welcome Mark!")    
+elif username == "bug":
+        print("hey bug!")
+else:
+    print("Access denied!")
+    
