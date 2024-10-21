88de3ad86e12ee4d81a24077d544494682f78a6c
dGarnett00 <davonta2222@gmail.com>
2024-10-21 13:48:23 -0400

    casting.py

--- Casting.py
+++ Casting.py
@@ -1,1 +1,15 @@
-print("hi") #comment added
+myScore = int (input("Your score: "))                              #using numbers in an effective way.
+if myScore > 100000:
+    print("Winner!")
+else:
+    print("try again")
+    
+    
+    
+    
+    
+    myPi = float(input("What is pi to 3dp?"))
+    if myPi == 3.142 :
+        print("Correct!")
+    else:
+        print("Incorrect!")
